fa58d9b03ba591d478b2cf1d48e33488034cbf99
Olga Zaitseva <ozaitseva@genos.hr>
2026-05-15 18:02:34 +0200

    Joined the three LaCyTools export files for EXP02 in a single 20260515_EXP02_combined.csv, it still contains contamination

---
+++ data/raw/R/00-EXP02-combining-data.R
@@ -0,0 +1,91 @@
+library(readxl)
+library(dplyr)
+library(tidyr)
+library(data.table)
+
+
+# load all three data sources
+
+X <- fread("../2025-04-11-1513Z_Summary.csv")
+X1 <- fread("../2025-04-11-1541Z_Summary.txt")
+
+colnames(X1)[1] <- "Sample"
+colnames(X1)[2] <- "Charge"
+
+## check if sample list is the same
+
+setdiff(unique(X$Sample), unique(X1$Sample))
+
+
+setdiff(unique(X1$Sample), unique(X$Sample))
+
+### for now remove the standards because there is an issue with stand_03 integration
+
+X <- X %>%
+  filter(!(grepl("stand", Sample)))
+
+X1 <- X1 %>%
+  filter(!(grepl("stand", Sample)))
+
+
+## check which colnames overlap between the 2 documents
+
+overlap <- colnames(X1)[colnames(X1) %in% colnames(X)]
+
+overlap_glycans <- overlap[grepl("IgG", overlap)]
+
+## Columns with the same name in X need to be replaced with the same columns from X1
+## X is a datatabe, hence I have to subset it in a stupid way
+
+X <- X[,!(names(X) %in% overlap_glycans),  with = FALSE]
+
+X_ <- merge(X, X1)
+
+## load X2
+
+X2 <- fread("../2025-04-28-1117Z_Summary.txt")
+## check if sample list is the same
+
+setdiff(unique(X$Sample), unique(X2$Sample))
+
+
+setdiff(unique(X2$Sample), unique(X$Sample))
+
+### for now remove the standards because there is an issue with stand_03 integration
+
+
+X2 <- X2 %>%
+  filter(!(grepl("stand", Sample)))
+
+
+## check which colnames overlap between the 2 documents
+
+overlap <- colnames(X2)[colnames(X2) %in% colnames(X_)]
+
+###no overlapping columns with glycans and rename it to X
+
+X <- merge(X_, X2)
+
+
+# drop columns with IgGII1 
+igg2 <- names(X)[grepl("IgGII1", names(X))]
+X_ <- X[,!(names(X) %in% igg2),  with = FALSE]
+
+X <- X_
+
+X <- X %>%
+  relocate(Sample)
+
+## add sample info
+samples <- fread("../EXP02_samples.csv")
+
+X <- X %>%
+  mutate(genos_id = gsub("_2-.*$", "", Sample))
+
+X <- merge(X, samples,by = "genos_id")
+
+X <- X %>%
+  relocate(Sample, Charge,genos_id,allotype,construct)
+
+
+fwrite(X, file = "./output/20260515_EXP02_combined.csv")
